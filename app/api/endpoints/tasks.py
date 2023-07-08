87345d945eadc82f0f7c5db4105b76344a324f22
vadim <vadimbor93@mail.ru>
2023-07-08 23:40:18 +0300

    Добавлена валидация по модели

--- app/api/endpoints/tasks.py
+++ app/api/endpoints/tasks.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from app.api.validators import check_build_is_exists
+from app.models.build import BuildBase
 from app.services.convert_yaml import build_objects, task_objects
 from app.services.get_tasks import get_sorted_tasks
 
@@ -15,7 +16,8 @@ router = APIRouter()
     description="Эндпоинт для получения списка заданий (tasks). ",
 )
 async def get_tasks(
-    build_name: str,
+    # build_name: str,
+    build: BuildBase,
 ):
-    build = check_build_is_exists(build_objects, build_name)
+    build = check_build_is_exists(build_objects, build.name)
     return get_sorted_tasks(build, task_objects)
